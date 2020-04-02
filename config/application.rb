846af39d30d67b55432218ee147fe132e0e23acd
come1120 <61959057+come1120@users.noreply.github.com>
2020-04-02 21:28:37 +0900

    0403

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-    end
+    config.i18n.default_locale = :ja
   end
 end
