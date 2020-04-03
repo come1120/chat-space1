4e0fbff2dd2f84615b25d25712713f1bfdf6521f
come1120 <61959057+come1120@users.noreply.github.com>
2020-04-03 22:13:54 +0900

    0403
    form/new/edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  
   def edit
   end
 
   def update
     if current_user.update(user_params)
       redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
     else
       render :edit
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
